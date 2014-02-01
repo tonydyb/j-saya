02258ad2c4824b11bceaca774f56481674653d69
enjyoban <enjyoban@gmail.com>
2014-02-01 21:56:51 +0900

    no message

--- db.rb
+++ db.rb
@@ -6,8 +6,8 @@ require 'sqlite3'
 require_relative 'yahoojp_finance_scraping.rb'
 require_relative 'contrib/ruby-progressbar-0.9/progressbar'
 
-@code_range  = [1301,9999]
-@code_toyota = '7203.T'
+$code_range  = [1301,9999]
+$code_toyota = '7203.T'
 
 class String
   def extract_value
@@ -24,11 +24,11 @@ class ConfigError < StandardError; end
 
 def read_configure
  	conf = YAML.load_file 'configure.yaml'
-	@db_sdate = conf['DB']['START_DATE']
-  @work_dir = conf['WORK_DIR']
-  if !File.exist?(@work_dir) 
-    Dir.mkdir @work_dir 
-  elsif File.ftype(@work_dir) != "directory"
+	$db_sdate = conf['DB']['START_DATE']
+  $work_dir = conf['WORK_DIR']
+  if !File.exist?($work_dir) 
+    Dir.mkdir $work_dir 
+  elsif File.ftype($work_dir) != "directory"
     raise ConfigError, "WORK_DIR must be directory"
   end
 end 
@@ -62,28 +62,28 @@ def quote (code)
 end
 
 def sqlite_connect
-  dbname = @work_dir + "/stock.sqlite3"
+  dbname = $work_dir + "/stock.sqlite3"
   if File.exist?(dbname) 
-     @con = SQLite3::Database.open(dbname)
+     $con = SQLite3::Database.open(dbname)
   else
-     @con = SQLite3::Database.new(dbname) 
-     @opts[:s] = true
+     $con = SQLite3::Database.new(dbname) 
+     $opts[:s] = true
   end
 end
 
 def renew_summary_table
-  q = quote(@code_toyota)
+  q = quote($code_toyota)
   toushou1 = q[:market][1]	
 
   command =<<SQL
 select name from sqlite_master where type = 'table'
 SQL
 
-  exist_tables = @con.execute(command).flatten
+  exist_tables = $con.execute(command).flatten
 
   tgt_table = 'toushou1_summary_column_jp'
   if exist_tables.include? tgt_table
-    @con.execute("drop table #{tgt_table}")
+    $con.execute("drop table #{tgt_table}")
   end
   command =<<SQL
 create table #{tgt_table} 
@@ -99,14 +99,14 @@ market_cap text
 )
 SQL
   
-  @con.execute command
+  $con.execute command
 
   pcommand =<<SQL
   insert into #{tgt_table} VALUES (?,?,?,?,?,?,?,?)
 SQL
  
 begin
-  stmt = @con.prepare pcommand
+  stmt = $con.prepare pcommand
   
 	stmt.execute q[:name][0],
 	q[:symbol][0],
@@ -122,7 +122,7 @@ end
 
 tgt_table = 'toushou1_summary'
   if exist_tables.include? tgt_table
-    @con.execute("drop table #{tgt_table}")
+    $con.execute("drop table #{tgt_table}")
   end
   command =<<SQL
 create table #{tgt_table} 
@@ -138,18 +138,18 @@ market_cap integer
 )
 SQL
 
-  @con.execute command
+  $con.execute command
 
   pcommand =<<SQL
   insert into #{tgt_table} VALUES (?,?,?,?,?,?,?,?)
 SQL
  
 begin
-  stmt = @con.prepare pcommand
+  stmt = $con.prepare pcommand
 
-	pbar = ProgressBar.new('R. summaries',@code_range.last-@code_range.first)
+	pbar = ProgressBar.new('R. summaries',$code_range.last-$code_range.first)
 
-	for c in @code_range.first..@code_range.last	
+	for c in $code_range.first..$code_range.last	
 		pbar.inc
 		code = c.to_s + ".T"
 
@@ -176,7 +176,7 @@ def remove_all_histories
   command =<<SQL
 select name from sqlite_master where type = 'table'
 SQL
-  exist_tables = @con.execute(command).flatten
+  exist_tables = $con.execute(command).flatten
 
   delete_tables = []
 	exist_tables.each { |x| 
@@ -188,7 +188,7 @@ SQL
 	pbar = ProgressBar.new('D. histories',delete_tables.size)
 	delete_tables.each { |x|
 		pbar.inc
-    @con.execute("drop table #{x}")
+    $con.execute("drop table #{x}")
 	}
 	puts ''
 end
@@ -197,7 +197,7 @@ def renew_histories
   command =<<SQL
 select name from sqlite_master where type = 'table'
 SQL
-  exist_tables = @con.execute(command).flatten
+  exist_tables = $con.execute(command).flatten
 
   t_tables = []
 	exist_tables.each { |x| 
@@ -206,7 +206,7 @@ SQL
 		end
 	}
 
-	symbols = @con.execute('select symbol from toushou1_summary').flatten
+	symbols = $con.execute('select symbol from toushou1_summary').flatten
 
 	pbar = ProgressBar.new('C. histories',symbols.size)
 
@@ -214,20 +214,20 @@ SQL
 	symbols.each { |code| 
     pbar.inc
     code = code.to_i
-		if code < @code_range[0] || @code_range[1] < code
+		if code < $code_range[0] || $code_range[1] < code
 			next
 		end 
-		name,sdate = "StockPrice#{code}", @db_sdate
+		name,sdate = "StockPrice#{code}", $db_sdate
 		
     if t_tables.include? name
-			day = @con.execute("select max(date) from #{name}").flatten[0]
+			day = $con.execute("select max(date) from #{name}").flatten[0]
       if day == nil
         next
       end
 			sy,sm,sd = day.split('-').collect { |x| x.to_i }
 	    sdate = Date.new(sy,sm,sd) + 1 	
 	  else 
-       @con.execute "create table #{name} (date text, close integer)"
+       $con.execute "create table #{name} (date text, close integer)"
     end
  
     case sdate.wday
@@ -246,7 +246,7 @@ SQL
       x.unshift [Date.new(sy,sm,sd).strftime("%Y-%m-%d"), y[6].delete(',')]
 		}
 begin
-		stmt = @con.prepare "insert into #{name} VALUES (?,?)"
+		stmt = $con.prepare "insert into #{name} VALUES (?,?)"
 		for o in revo
 			stmt.execute o[0],o[1] 
 		end
@@ -256,25 +256,25 @@ ensure
 end
   }
 	#t_tables.each { |x|
-	#@con.execute("drop table #{x}")
+	#$con.execute("drop table #{x}")
 	#}
 	puts ''
 end
 
 opt = OptionParser.new
-@opts = {}
+$opts = {}
 Version = '1.0.1'
 Description = {:s =>"renew summaries of stocks",
                :t =>"renew histrical prices of stocks",
                :v =>"show version"}
 
 Description.each { |k,v|
-	opt.on("-#{k}", v) {|v| @opts[k] = true }
+	opt.on("-#{k}", v) {|v| $opts[k] = true }
 }
 
 opt.parse!(ARGV)
 
-if @opts[:v]
+if $opts[:v]
   puts "version 0.1.0"
   exit
 end
@@ -285,11 +285,11 @@ read_configure
 
 sqlite_connect
 
-if @opts[:s]
+if $opts[:s]
   renew_summary_table
 end
 
-if @opts[:t]
+if $opts[:t]
   remove_all_histories
 end
 
@@ -302,8 +302,8 @@ rescue => e
     puts x
   }
 ensure
-  if @con && !@con.closed?
-    @con.close
+  if $con && !$con.closed?
+    $con.close
    end
 end
 
--- select.rb
+++ select.rb
@@ -24,11 +24,11 @@ class Stock
     tgt_table = "StockPrice#{summary[:symbol]}"
     command =<<SQL
 select date, close from #{tgt_table} 
-    where date <= '#{@idate}' 
+    where date <= '#{$idate}' 
     order by date desc 
     limit #{$period} 
 SQL
-    history = @con.execute(command).reverse.collect { |x|
+    history = $con.execute(command).reverse.collect { |x|
       sy,sm,sd = x[0].split('-').collect {|y| y.to_i }
       [Date.new(sy,sm,sd).jd,x[1]]
     }
@@ -41,8 +41,8 @@ end
 def read_configure
   	conf = YAML.load_file 'configure.yaml'
     select = conf['SELECT']
-    @work_dir = conf['WORK_DIR']
-    if !File.exist?(@work_dir) || File.ftype(@work_dir) != "directory"
+    $work_dir = conf['WORK_DIR']
+    if !File.exist?($work_dir) || File.ftype($work_dir) != "directory"
       raise ConfigError, "invalid WORK_DIR"
     end
     $period = select['PERIOD']
@@ -51,18 +51,19 @@ def read_configure
 end
 
 def sqlite_connect
-  dbname = @work_dir + "/stock.sqlite3"
-  @con = SQLite3::Database.open(dbname)
+  dbname = $work_dir + "/stock.sqlite3"
+  $con = SQLite3::Database.open(dbname)
 end
 
 def read_stocks
-  @stocks = []
+  $stocks = []
   colums = [:name,:symbol,:market_cap]
    
   command =  "select #{colums.join(',')} from toushou1_summary"
 
-  res = @con.execute(command)
+  res = $con.execute(command)
 	pbar = ProgressBar.new('Read stocks',res.size)
+
   res.each { |s|
     pbar.inc
     summary = {} 
@@ -70,8 +71,9 @@ def read_stocks
        summary[x] = s[ind]
     }
     mked = Stock::make(summary)
-    if  mked != nil; @stocks << mked; end
+    $stocks << mked unless mked == nil
   }
+  puts ''
 end
 
 opt = OptionParser.new
@@ -81,9 +83,9 @@ opt.on("-v","show version") { |v|
   exit
 }
 
-@idate = Date.today
+$idate = Date.today
 opt.on("-d VAL", "target date(%y-%m-%d)") { |v|  
-   	@idate = Date.parse(v)
+   	$idate = Date.parse(v)
 }
 
 opt.parse!(ARGV)
@@ -94,13 +96,13 @@ read_configure
 sqlite_connect
 read_stocks
 
-o = File.open("#{@work_dir}/selected.txt",'w')
-o.puts @idate.to_s
+o = File.open("#{$work_dir}/selected.txt",'w')
+o.puts $idate.to_s
 
-pbar = ProgressBar.new('Select pairs',@stocks.size*(@stocks.size-1)/2)
+pbar = ProgressBar.new('Select pairs',$stocks.size*($stocks.size-1)/2)
 
-@stocks.each { |a|
-	@stocks.each { |b|
+$stocks.each { |a|
+	$stocks.each { |b|
 		if a.code >= b.code; next; end
     pbar.inc
 
@@ -135,8 +137,8 @@ rescue => e
     puts x
   }
 ensure
-  if @con && !@con.closed?
-    @con.close
+  if $con && !$con.closed?
+    $con.close
   end
 end
 
